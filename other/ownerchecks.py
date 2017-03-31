5f60df79cd9daa22b9cda983d7507dc6c848a9ed
S Stewart <hi@youneeda.pw>
2017-03-30 20:51:36 -0500

    Finalized

---
+++ other/ownerchecks.py
@@ -0,0 +1,6 @@
+from discord.ext import commands
+import json
+
+config = json.loads(open('config.json').read())
+def is_owner():
+  return commands.check(lambda ctx: ctx.message.author.id == config['owner'])

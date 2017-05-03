4a6116632d70247f421261471757213062f027d4
S Stewart <poweredby@linuxmail.org>
2017-05-02 21:13:14 -0400

    [ownerchecks] return message beta

--- other/ownerchecks.py
+++ other/ownerchecks.py
@@ -4,3 +4,6 @@ import json
 config = json.loads(open('config.json').read())
 def is_owner():
   return commands.check(lambda ctx: ctx.message.author.id == config['ownerid'])
+  except:
+    noperms = discord.Embed(name='Error', description='You have no permissions to use this command.')
+    return commands.bot.say(embed=noperms)

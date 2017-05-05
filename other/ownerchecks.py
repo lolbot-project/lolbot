f5a1929926bc2ce6c3b2d2c1256cf3000b4717aa
S Stewart <poweredby@linuxmail.org>
2017-05-05 15:08:25 -0400

    [ownerchecks] experimental fix

--- other/ownerchecks.py
+++ other/ownerchecks.py
@@ -3,7 +3,8 @@ import json
 
 config = json.loads(open('config.json').read())
 def is_owner():
-  return commands.check(lambda ctx: ctx.message.author.id == config['ownerid'])
-  except:
+  try:
+    return commands.check(lambda ctx: ctx.message.author.id == config['ownerid'])
+  except discord.ext.commands.errors.CheckFailure:
     noperms = discord.Embed(name='Error', description='You have no permissions to use this command.')
     return commands.bot.say(embed=noperms)

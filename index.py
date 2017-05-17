99252d2fd849a964e937ddf759821436389c81bc
S Stewart <hi@youneeda.pw>
2017-05-16 22:51:32 -0500

    [stats] uptime no work

--- index.py
+++ index.py
@@ -129,11 +129,10 @@ async def shibe(ctx):
 async def stats(ctx):
   """A few stats."""
   # get_owner = bot.get_user_info(config['ownerid'])
-  statUptime = str(datetime.datetime.now() - ctx.bot.time_started).split(".")[0]
   statInfo = await ctx.bot.application_info()
   statEmbed = discord.Embed(title='lolbot stats', description='This bot is powered by [lolbot](https://github.com/memework/lolbot),'
   ' a fast and powerful Python bot.', colour=0x690E8)
-  statEmbed.add_field(name='Owner', value='' + statInfo.owner.mention + '\nUser ID:' + statInfo.owner.user.id)
+  statEmbed.add_field(name='Owner', value=statInfo.owner.mention)
   statEmbed.add_field(name='Python', value=sys.version)
   statEmbed.add_field(name='Servers', value=len(bot.guilds))
   statEmbed.add_field(name='Uptime', value=statUptime)

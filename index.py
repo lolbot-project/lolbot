c09a1793aa1587d68096af84625bbc05cd50d8bf
S Stewart <poweredby@linuxmail.org>
2017-05-16 23:53:07 -0400

    abort

--- index.py
+++ index.py
@@ -129,11 +129,7 @@ async def shibe(ctx):
 async def stats(ctx):
   """A few stats."""
   # get_owner = bot.get_user_info(config['ownerid'])
-<<<<<<< HEAD
-  statUptime = str(datetime.datetime.now() - ctx.bot.time_started).split(".")[0]
-=======
   statUptime = str(datetime.now() - ctx.bot.time_started).split(".")[0]
->>>>>>> c23363bee1668f5123c5436afe7d64f46430cf07
   statInfo = await ctx.bot.application_info()
   statEmbed = discord.Embed(title='lolbot stats', description='This bot is powered by [lolbot](https://github.com/memework/lolbot),'
   ' a fast and powerful Python bot.', colour=0x690E8)

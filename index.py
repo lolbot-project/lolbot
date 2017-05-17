3bf40029b7d550ffdbd57dd3336c6b47093148b7
S Stewart <hi@youneeda.pw>
2017-05-16 22:13:18 -0500

    [test] test

--- index.py
+++ index.py
@@ -2,6 +2,16 @@
 lolbot - by S Stewart
 Under MIT License
 Copyright (c) S Stewart 2017
+
+I would like to make credit to a few
+bot makers that have helped me over
+the past few months to make lolbot
+what it is now.
+
+slice
+fn230
+
+Both have provided good help to me.
 """
 # -*- coding: utf-8 -*-
 import discord
@@ -119,6 +129,7 @@ async def shibe(ctx):
 async def stats(ctx):
   """A few stats."""
   # get_owner = bot.get_user_info(config['ownerid'])
+  uptime = str(datetime.datetime.now() - ctx.bot.time_started).split(".")[0]
   statEmbed = discord.Embed(title='lolbot stats', description='This bot is powered by [lolbot](https://github.com/memework/lolbot),'
   ' a fast and powerful Python bot.', colour=0x690E8)
   statEmbed.add_field(name='Owner', value=await ctx.bot.application_info())

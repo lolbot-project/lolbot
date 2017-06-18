59b7c1cace221b36c61c1334be58e48966e3fe66
S Stewart <poweredby@linuxmail.org>
2017-06-17 22:24:45 -0500

    [eval] actually, I just made a mistake nvm

--- index.py
+++ index.py
@@ -149,6 +149,7 @@ async def evalboi(ctx, *, code: str):
     else:
       stdout = io.StringIO()
       with outredir(stdout):
+        wrapped_code = 'async def func():\n' + textwrap.indent(code, '  ')
         lol = exec(compile(code, '<exec>', 'exec'), env)
   except Exception as e:
     evalError = discord.Embed(title='Error', description='You made non-working'

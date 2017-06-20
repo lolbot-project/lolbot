079a161cfe9f778c7734fdad5df0fd9c282ded17
S Stewart <poweredby@linuxmail.org>
2017-06-19 21:37:47 -0500

    [core, idk] fix indents

--- index.py
+++ index.py
@@ -19,31 +19,31 @@ from discord.ext import commands
 bot = commands.AutoShardedBot(command_prefix='^', description=description)
 bot.session = aiohttp.ClientSession(loop=bot.loop)
 
-  @bot.command(hidden=True)
-  @commands.is_owner()
-  async def reboot(self, ctx):
-    """Duh. Owner only"""
-    rebootPend = discord.Embed(title='Rebooting', description='Rebooting...', colour=0x690E7)
-    await ctx.send(embed=rebootPend)
-    try:
-      check_output(['sh', 'bot.sh'])
-      logging.info('reboot requested')
-      logging.info('hoping it goes well now')
-    except:
-      logging.error('pls tell lold to fix his code')
-    else:
-      await bot.logout()
+@bot.command(hidden=True)
+@commands.is_owner()
+async def reboot(self, ctx):
+  """Duh. Owner only"""
+  rebootPend = discord.Embed(title='Rebooting', description='Rebooting...', colour=0x690E7)
+  await ctx.send(embed=rebootPend)
+  try:
+    check_output(['sh', 'bot.sh'])
+    logging.info('reboot requested')
+    logging.info('hoping it goes well now')
+  except:
+    logging.error('pls tell lold to fix his code')
+  else:
+    await bot.logout()
 
-  @bot.command(hidden=True)
-  @commands.is_owner()
-  async def game(self, ctx, *, game: str):
-    """Changes playing status"""
-    try:
-      await bot.change_presence(game=discord.Game(name=game + ' | ^help | v6.1'))
-    except:
-      await ctx.send('Something went wrong - check the console for details')
-    else:
-      await ctx.send(':white_check_mark: Changed game')
+@bot.command(hidden=True)
+@commands.is_owner()
+async def game(self, ctx, *, game: str):
+  """Changes playing status"""
+  try:
+     await bot.change_presence(game=discord.Game(name=game + ' | ^help | v6.1'))
+  except:
+    await ctx.send('Something went wrong - check the console for details')
+  else:
+    await ctx.send(':white_check_mark: Changed game')
 
 @bot.event
 async def on_ready():

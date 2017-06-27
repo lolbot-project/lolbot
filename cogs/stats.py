c8737eec56c3383007656c09168ba3f7b6a47b19
S Stewart <poweredby@linuxmail.org>
2017-06-26 21:03:25 -0500

    [cogs, stats] Fix posting by using a different session

--- cogs/stats.py
+++ cogs/stats.py
@@ -1,11 +1,14 @@
 import json
 import logging
+import aiohttp
 
+logging.basicConfig(format='[%(levelname)s] - %(message)s', level=logging.INFO)
 config = json.load(open('config.json'))
 
 class Stats:
   def __init__(self, bot):
     self.bot = bot
+    self.session = aiohttp.ClientSession(loop=self.bot.loop)
   # danny code frankenstein :P
   async def botstats(self):
     payload = json.dumps({
@@ -21,13 +24,13 @@ class Stats:
     }
     dbots_url = 'https://bots.discord.pw/api/bots/' + config['botid'] + '/stats'
     dbl_url = 'https://discordbots.org/api/bots/' + config['botid'] + '/stats'
-    async with self.bot.session.post(dbl_url, data=payload, headers=dbl_headers) as dbl_resp:
+    async with self.session.post(dbl_url, data=payload, headers=dbl_headers) as dbl_resp:
       logging.info('dbl: posted with code' + str(dbl_resp.status))
-    async with self.bot.session.post(dbots_url, data=payload, headers=headers) as resp:
+    async with self.session.post(dbots_url, data=payload, headers=headers) as resp:
       logging.info('dbots: posted with code' + str(resp.status))
  
   async def on_guild_join( self, guild ):
-    logging.info('Joined guild ' + str(guild.name) + ' ID: ' + str(guild.id))
+    logging.info('Joined guild "' + str(guild.name) + '" ID: ' + str(guild.id))
     await botstats()
 
   async def on_guild_remove( self, guild ):

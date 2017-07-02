917c03099a1ad27e2336f46225ed6a923dbc9bb9
S Stewart <poweredby@linuxmail.org>
2017-07-02 01:32:44 -0500

    [ci, tox, setuptools] o

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup
 setup(name='lolbot', version='6.2', description='A fun bot',
       url='https://github.com/xshotD/lolbot', author='S Stewart',
-      python_requires='>=3.6', install_requires=['https://github.com/rapptz'
-      '/discord.py@rewrite'])
+      python_requires='>=3.6')
 
 

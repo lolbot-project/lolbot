8b82616f23dde6e7031416d77041bab1d2ccc055
S Stewart <poweredby@linuxmail.org>
2017-07-02 00:34:36 -0500

    [core] Fix the error handles lul

--- index.py
+++ index.py
@@ -39,11 +39,11 @@ async def on_ready():
   logging.info('Playing status changed')
 
 @bot.event
-async def on_command_error(ctx):
+async def on_command_error(ctx, error):
   if isinstance(error, commands.errors.CheckFailure):
-    await ctx.send(f'{random.choice(checkfail)}')
+      await ctx.send(f'Permissions error: {random.choice(checkfail)}')
   elif isinstance(error, commands.errors.BadArgument):
-    await ctx.send(f'{random.choice(badargs)}')
+      await ctx.send(f'Bad argument error: {random.choice(badargs)}')
 
 if __name__ == '__main__':
   for ext in exts:

a71d5190ba4ed34894ab57d1dfb97c2c1ced38b1
S Stewart <poweredby@linuxmail.org>
2017-07-02 15:33:41 -0500

    [reqs, ci, tox, core] idfk what I did

--- index.py
+++ index.py
@@ -41,7 +41,7 @@ async def on_command_error(ctx, error):
   if isinstance(error, commands.errors.CheckFailure):
       await ctx.send(f'Permissions error: {random.choice(checkfail)}')
   elif isinstance(error, commands.errors.BadArgument):
-      await ctx.send(f'Bad argument error: {random.choice(badarg)}')
+      await ctx.send(f'Bad argument error: {random.choice(badargs)}')
 
 if __name__ == '__main__':
   for ext in exts:

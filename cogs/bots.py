8265d29df07ff5cda394ddb3175ad3b6c245a2d7
S Stewart <poweredby@linuxmail.org>
2017-07-05 06:01:47 -0500

    [cogs, dbots] push some update

--- cogs/bots.py
+++ cogs/bots.py
@@ -22,8 +22,13 @@ class DBots:
   @dbots.command(name='owner')
   async def dbots_owner(self, ctx, wanted: discord.Member):
     async with self.session.get('https://bots.discord.pw/api/bots/' + str(wanted.id), headers=self.headers) as info:
-      botinfo = await info.json()
-      await ctx.send('The owner of ' + str(wanted.mention) + 'is ' + str(botinfo['owner_ids'][0]))
+      if info.status == 200:
+        botinfo = await info.json()
+        await ctx.send('The owner of ' + str(wanted.mention) + ' is ' + str(botinfo['owner_ids'][0]))
+      else if info.status == 404:
+        await ctx.send('That bot is not in Discord Bots!')
+      else:
+        await ctx.send('A error happened somewhere - sorry about that.')
 
 def setup(bot):
   bot.add_cog(DBots(bot))

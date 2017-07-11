8266926b01ec1f2149b195f399d5f0b1bd389c8e
S Stewart <poweredby@linuxmail.org>
2017-07-11 01:22:54 -0500

    [cogs, dbots] add in checking for dbots api keys

--- cogs/bots.py
+++ cogs/bots.py
@@ -24,15 +24,18 @@ class DBots:
   async def dbots_owner(self, ctx, wanted: discord.Member):
     async with self.session.get('https://bots.discord.pw/api/bots/' + str(wanted.id), headers=self.headers) as info:
       """Gets the owner of a bot"""
-      if info.status == 200:
-        botinfo = await info.json()
-        if len(botinfo['owner_ids']) == 1:
+      if self.config['dbots'] == '':
+        await ctx.send('This bot does not have a DBots API key set up.')
+      else:
+        if info.status == 200:
+          botinfo = await info.json()
+          if len(botinfo['owner_ids']) == 1:
             owner = self.bot.get_user(botinfo['owner_ids'][0])
             await ctx.send(f'The bot {wanted.mention} is owned by {owner.name}#{owner.discriminator}')
-      elif info.status == 404:
-        await ctx.send('That bot is not in Discord Bots!')
-      elif info.status == 504:
-        await ctx.send('Server timed out - this is a fault with Discord Bots. Sorry!')
+          elif info.status == 404:
+            await ctx.send('That bot is not in Discord Bots!')
+          elif info.status == 504:
+            await ctx.send('Server timed out - this is a fault with Discord Bots. Sorry!')
 
 def setup(bot):
   bot.add_cog(DBots(bot))

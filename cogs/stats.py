31154e8194cf6680ee26bbc4b5c72b79a9fbc639
S Stewart <poweredby@linuxmail.org>
2017-07-15 22:07:08 -0500

    [cogs, stats] house posts in diff functions

--- cogs/stats.py
+++ cogs/stats.py
@@ -6,11 +6,13 @@ logging.basicConfig(format='[%(levelname)s] - %(message)s', level=logging.INFO)
 config = json.load(open('config.json'))
 
 class Stats:
-  async def listpost(self, bot):
-    payload = json.dumps({
-      'server_count': len(bot.guilds)
-    })
+  def __init__(self, bot):
+    self.bot = bot
     session = aiohttp.ClientSession()
+    self.payload = json.dumps({
+      'server_count': len(self.bot.guilds)
+    })
+  async def dbotspost(self):
     if config['dbotsorg'] == False:
       logging.info('not posting to discordbots.org')
     else:
@@ -19,12 +21,13 @@ class Stats:
         'Content-Type': 'application/json'
       }
       dbl_url = 'https://discordbots.org/api/bots/' + config['botid'] + '/stats'
-      async with session.post(dbl_url, data=self.payload, headers=dbl_headers) as dbl_resp:
+      async with self.session.post(dbl_url, data=self.payload, headers=dbl_headers) as dbl_resp:
         if dbl_resp.status == 200:
           logging.info('dbl: posted!')
         else:
-          logging.info(f'dbl: something weird happened: code {dbl_resp.status}.')                                                   
+          logging.info(f'dbl: something weird happened: code {dbl_resp.status}.')
 
+  async def dblpost(self):
     if config['dbotspw'] == False:
       logging.info('not posting to bots.discord.pw')
     else:
@@ -33,21 +36,19 @@ class Stats:
         'Content-Type': 'application/json'
       }
       dbots_url = 'https://bots.discord.pw/api/bots/' + config['botid'] + '/stats'
-      async with session.post(dbots_url, data=payload, headers=headers) as resp:
-        assert resp.status == 200
-        logging.info('dbots: posted!')
-
-  def __init__(self, bot):
-    self.bot = bot
-    self.post = listpost
+      async with self.session.post(dbots_url, data=self.payload, headers=headers) as resp:
+          if resp.status == 200:
+            logging.info('dbots: posted!')
 
   async def on_guild_join( self, guild ):
     logging.info('Joined guild "' + str(guild.name) + '" ID: ' + str(guild.id))
-    await listpost()
+    await dbotspost()
+    await dblpost()
 
   async def on_guild_remove( self, guild ):
     logging.info('Left ' + str(guild.name))
-    await listpost()
+    await dbotspost()
+    await dblpost()
 
 def setup(bot):
   bot.add_cog(Stats(bot))

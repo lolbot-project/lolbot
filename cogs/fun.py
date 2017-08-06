b0a5837de9cad4d35fdfc2854048be970f7c9aa8
S Stewart <iamtheworst@programmer.net>
2017-08-06 04:36:39 -0500

    [cogs(fun,nekos)] Push out stuff

--- cogs/fun.py
+++ cogs/fun.py
@@ -6,12 +6,14 @@ from discord.ext import commands
 class Fun:
   def __init__(self, bot):
     self.bot = bot
-    self.session = aiohttp.ClientSession(loop=self.bot.loop)
+    self.ua = {
+            'User-Agent': 'lolbot (discord.py/aiohttp)'
+        }
 
   @commands.command()
   async def cat(self, ctx):
     """Random cat images. Awww, so cute! Powered by random.cat"""
-    async with self.session.get('https://random.cat/meow') as r:
+    async with self.bot.session.get('https://random.cat/meow', headers=self.ua) as r:
       if r.status == 200:
         js = await r.json()
         em = discord.Embed(name='random.cat', colour=0x690E8)
@@ -28,8 +30,10 @@ class Fun:
   @commands.command()
   async def dog(self, ctx):
     """Random dogs, by random.dog"""
-    async with self.session.get('https://random.dog/woof') as shibeGet:
-      if shibeGet.status == 200:
+    async with self.bot.session.get('https://random.dog/woof', headers=self.ua) as shibeGet:
+    load = await ctx.send('Fetching a dog...')
+    if shibeGet.status == 200:
+        load.delete()
         shibeImg = await shibeGet.text()
         shibeURL = 'https://random.dog/' + shibeImg
         if '.mp4' in str(shibeURL):
@@ -39,28 +43,58 @@ class Fun:
           shibeEmbed.set_image(url=shibeURL)
           await ctx.send(embed=shibeEmbed)
       else:
-        await ctx.send('Uh oh, I failed to get the picture for some reason')
+        load.delete()
+        await ctx.send(f'Something happened while fetching the picture (HTTP code {shibeGet.status})')
 
+    @commands.command()
+    async def lizard(self, ctx):
+        """Gets a random lizard"""
+        async with self.bot.session.get('https://nekos.life/api/lizard', headers=self.ua) as lizard:
+            load = await ctx.send('Fetching a lizard...')
+            if lizard.status == 200:
+                load.delete()
+                lizJS = lizard.json()
+                lizEm = discord.Embed(colour=0x690E8)
+                lizEm.set_image(lizJS['lizard'])
+                await ctx.send(embed=lizEm)
+            else:
+                load.delete()
+                await ctx.send(f'Something happened while fetching the lizard (HTTP code {lizard.status})')
 
-  @commands.command()
-  async def k(self, ctx):
-    """k"""
-    await ctx.send('k')
+    @commands.command()
+    async def why(self, ctx):
+        """Why _____?"""
+        load = await ctx.send('Fetching sentence...')
+        async with self.bot.session.get('https://nekos.life/api/why', headers=self.ua) as why:
+            if why.status == 200:
+                load.delete()
+                whyJS = why.json()
+                whyEm = discord.Embed(title=f'{ctx.author.name} wonders...',
+                        description=whyJS['why'], colour=0x690E8)
+                await ctx.send(embed=whyEm)
+            else:
+                load.delete()
+                await ctx.send(f'I wasn\'t able to fetch the sentence. (HTTP code {why.status})')
 
-  @commands.command()
-  async def fuck(self, ctx):
-    """fuck"""
-    await ctx.send('fuck')
+    @commands.command()
+    async def k(self, ctx):
+        """k"""
+        await ctx.send('k')
 
-  @commands.command(name='8ball')
-  async def an8ball(self, ctx, *, question: str):
-    pool = ['It is certain', 'Outlook good', 'You may rely on it', 'Ask again '
-    'later', 'Concentrate and ask again', 'Reply hazy, try again', 'My reply is '
-    'no', 'My sources say no']
-    ans = random.choice(pool)
-    emb = discord.Embed(title='The Magic 8-ball', description='**Question: ' +
-    str(question) + '**\nAnswer: ' + str(ans), colour=0x690E8)
-    await ctx.send(embed=emb)
+    @commands.command()
+    async def fuck(self, ctx):
+        """fuck"""
+        await ctx.send('fuck')
+
+    @commands.command(name='8ball')
+    async def an8ball(self, ctx, *, question: str):
+        pool = ['It is certain', 'Outlook good', 'You may rely on it', 'Ask again '
+        'later', 'Concentrate and ask again', 'Reply hazy, try again', 'My reply is '
+        'no', 'My sources say no']
+        ans = random.choice(pool)
+        emb = discord.Embed(title='The Magic 8-ball', description='**Question: ' +
+        str(question) + '**\nAnswer: ' + str(ans), colour=0x690E8)
+        await ctx.send(embed=emb)
 
 def setup(bot):
-  bot.add_cog(Fun(bot))
+    bot.add_cog(Fun(bot))
---
+++ cogs/nekos.py
@@ -0,0 +1,52 @@
+import discord
+import aiohttp
+import json
+from discord.ext import commands
+
+class Animemes:
+    def __init__(self, bot):
+        self.bot = bot
+        self.config = json.load(open('config.json'))
+        self.base = 'https://nekos.life/api/'
+        self.nekos = 'neko'
+        self.nlewd = 'lewd/neko'
+        self.header = {
+                'User-Agent': 'lolbot (discord.py/aiohttp)'
+        }
+
+    @commands.command()
+    async def neko(self, ctx):
+        """Shows a random neko picture"""
+        load = await ctx.send('Fetching a neko...')
+        async with self.bot.session.get(f'{self.base}{self.nekos}', headers=self.header) as neko:
+            load.delete()
+            if neko.status == 200:
+                img = await neko.json()
+                nekoEm = discord.Embed(colour=0x690E8)
+                nekoEm.set_image(img['neko'])
+                nekoEm.set_footer(f'nekos.life | *For lewd nekos use {self.config["prefix"]}lneko*')
+            else:
+                lol = await ctx.send('Something weird happened when I tried to fetch your neko.'
+                        f'(HTTP code {neko.status})')
+
+    @commands.command()
+    async def lneko(self, ctx):
+        """Shows a random lewd neko pic"""
+        if ctx.channel.is_nsfw():
+            load = await ctx.send('Fetching a lewd neko...')
+            async with self.bot.session.get(f'{self.base}{self.nlewd}', headers=self.header) as lneko:
+                load.delete()
+                if lneko.status == 200:
+                    img = await lneko.json()
+                    lnekoEm = discord.Embed(colour=0x690E8)
+                    lnekoEm.set_image(img['neko'])
+                    lnekoEm.set_footer(f'nekos.life | *For normal nekos use {self.config["prefix"]}neko*')
+                else:
+                    await ctx.send('Something weird happened when I tried to fetch your neko.'
+                            f'(HTTP code {lneko.status})')
+            else:
+                await ctx.send('You\'re not in a NSFW channel. Therefore, I cannot post a'
+                        'lewd neko to this channel.')
+
+def setup(bot):
+    bot.add_cog(Animemes(bot))

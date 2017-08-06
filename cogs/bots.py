54776443af25be9ddbd517a061a89bb064443ce6
S Stewart <iamtheworst@programmer.net>
2017-08-06 04:48:20 -0500

    [cogs(bots)] ree

--- cogs/bots.py
+++ cogs/bots.py
@@ -23,21 +23,21 @@ class DBots:
     @dbots.command(name='owner')
     async def dbots_owner(self, ctx, wanted: discord.Member):
         async with self.session.get('https://bots.discord.pw/api/bots/' + str(wanted.id), headers=self.headers) as info:
-        """Gets the owner of a bot"""
-        if self.config['dbots'] == '':
-            await ctx.send('This bot does not have a DBots API key set up.')
-        else:
-            if info.status == 200:
-                botinfo = await info.json()
-                bot = await self.bot.get_user(botinfo['user_id'])
-                # No multi-owner bot support yet
-                # Because doing that is hard for me tbh
-                owner = await self.bot.get_user(botinfo['owner_ids'][0])
-                ownerbed = discord.Embed(title=f'Who owns {bot.mention}?', description=f'{owner.mention} does!', colour=0x690E8)
-            elif info.status == 404:
-                await ctx.send('That bot is not in Discord Bots!')
-            elif info.status == 504:
-                await ctx.send('Server timed out - this is a fault with Discord Bots. Sorry!')
+            """Gets the owner of a bot"""
+            if self.config['dbots'] == '':
+                await ctx.send('This bot does not have a DBots API key set up.')
+            else:
+                if info.status == 200:
+                    botinfo = await info.json()
+                    bot = await self.bot.get_user(botinfo['user_id'])
+                    # No multi-owner bot support yet
+                    # Because doing that is hard for me tbh
+                    owner = await self.bot.get_user(botinfo['owner_ids'][0])
+                    ownerbed = discord.Embed(title=f'Who owns {bot.mention}?', description=f'{owner.mention} does!', colour=0x690E8)
+                elif info.status == 404:
+                    await ctx.send('That bot is not in Discord Bots!')
+                elif info.status == 504:
+                    await ctx.send('Server timed out - this is a fault with Discord Bots. Sorry!')
 
 def setup(bot):
   bot.add_cog(DBots(bot))

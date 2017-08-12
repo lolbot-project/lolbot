8f3fbadfe27ccabedc7040eb11cbc0b53f5de8bf
S Stewart <iamtheworst@programmer.net>
2017-08-12 17:25:13 -0500

    [cogs(stats)] Respect config options

--- cogs/stats.py
+++ cogs/stats.py
@@ -10,47 +10,57 @@ class Stats:
         self.config = json.load(open('config.json'))
 
     async def dblpost(self):
-        headers = {
-            'Authorization': self.config['dbl'],
-            'Content-Type': 'application/json'
-        }
-        data = {
-            'server_count': len(self.bot.guilds),
-            'shard_count': len(self.bot.shards)
-        }
-        info = await self.bot.application_info()
-        req = await self.bot.session.post(f'https://discordbots.org/api/bots/{info.id}/stats', data=json.dumps(data), headers=headers)
-        status = req.status
-        if status == 200:
-            logging.info('poster[dbl]: done')
-        else:
-            resp = await req.text()
-            logging.error(f'poster[dbl]: oops (code {status})')
-            logging.error(f'poster[dbl]: response: {resp}')
-
-    async def dpwpost(self):
-        headers = {
-                'Authorization': self.config['dbots'],
+        if self.config['dbl'] != "":
+            headers = {
+                'Authorization': self.config['dbl'],
                 'Content-Type': 'application/json'
             }
-        data = {
+            data = {
                 'server_count': len(self.bot.guilds),
                 'shard_count': len(self.bot.shards)
             }
-        info = await self.bot.application_info()
-        req = await self.bot.session.post(f'https://bots.discord.pw/api/bots/{info.id}/stats', data=json.dumps(data), headers=headers)
-        status = req.status
-        if status == 200:
-            logging.info('poster[dbots]: done')
+            info = await self.bot.application_info()
+            req = await self.bot.session.post(f'https://discordbots.org/api/bots/{info.id}/stats', data=json.dumps(data), headers=headers)
+            status = req.status
+            if status == 200:
+                logging.info('poster[dbl]: done')
+            else:
+                resp = await req.text()
+                logging.error(f'poster[dbl]: oops (code {status})')
+                logging.error(f'poster[dbl]: response: {resp}')
         else:
-            resp = await req.text()
-            logging.error(f'poster[dbots]: oops (code {status})')
-            logging.error(f'poster[dbots]: response: {resp}')
+            pass
+
+    async def dpwpost(self):
+        if self.config['dbots'] != "":
+            headers = {
+                'Authorization': self.config['dbots'],
+                'Content-Type': 'application/json'
+            }
+            data = {
+                'server_count': len(self.bot.guilds),
+                'shard_count': len(self.bot.shards)
+            }
+            info = await self.bot.application_info()
+            req = await self.bot.session.post(f'https://bots.discord.pw/api/bots/{info.id}/stats', data=json.dumps(data), headers=headers)
+            status = req.status
+            if status == 200:
+                logging.info('poster[dbots]: done')
+            else:
+                resp = await req.text()
+                logging.error(f'poster[dbots]: oops (code {status})')
+                logging.error(f'poster[dbots]: response: {resp}')
 
     async def post(self):
-        logging.info('poster: starting post')
-        await self.dblpost()
-        await self.dpwpost()
+        logging.info('poster: starting')
+        if self.config['dbotsorg']:
+            await self.dblpost()
+        else:
+            pass
+        if self.config['dbotspw']:
+            await self.dpwpost()
+        else:
+            pass
         logging.info('poster: done')
 
     async def on_guild_join( self, guild ):

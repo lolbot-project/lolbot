f731eaa274bee24973a7583801e0de38950200c3
S Stewart <iamtheworst@programmer.net>
2017-08-24 22:40:52 -0500

    [cogs(owner)] 100 line cogs

--- cogs/owner.py
+++ cogs/owner.py
@@ -32,6 +32,39 @@ class Owner:
 
     @commands.command(hidden=True)
     @commands.is_owner()
+    async def load(self, ctx, extension_name : str):
+        """Loads an extension."""
+        try:
+            self.bot.load_extension("ext." + extension_name)
+        except Exception as e:
+            await ctx.send(f'```py\n{traceback.format_exc()}\n```')
+            return
+        log.info(f'Loaded {extension_name}')
+        await ctx.send(f':ok_hand: `{extension_name}` loaded.')
+
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def unload(self, ctx, extension_name : str):
+        """Unloads an extension."""
+        self.bot.unload_extension('ext.' + extension_name)
+        log.info(f'Unloaded {extension_name}')
+        await ctx.send(f':ok_hand: `{extension_name}` unloaded.')
+
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def reload(self, ctx, extension_name : str):
+        """Reloads an extension"""
+        try:
+            self.bot.unload_extension('ext.' + extension_name)
+            self.bot.load_extension('ext.' + extension_name)
+        except Exception as err:
+            await ctx.send(f'```{traceback.format_exc()}```')
+            return
+        log.info(f'Reloaded {extension_name}')
+        await ctx.send(f':ok_hand: Reloaded `{extension_name}`')
+
+    @commands.command(hidden=True)
+    @commands.is_owner()
     async def game(self, ctx, *, game: str):
         """Changes playing status"""
         try:

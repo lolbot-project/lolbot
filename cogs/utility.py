af4cb1081ac732d059e43a1156110316b8983c71
S Stewart <iamtheworst@programmer.net>
2017-08-28 21:26:54 -0500

    [cogs(utility)] Make ping work(?)

--- cogs/utility.py
+++ cogs/utility.py
@@ -50,10 +50,8 @@ class Utility:
         ping = await ctx.send('ping')
         after = time.monotonic()
         msLogic = round((after - before) * 1000, 2)
-        ms = discord.Embed(title='Pong.', description='Response time was '
-        'a nice ' + f'{msLogic}ms' + '!', colour=0x690E8)
-        await ping.delete()
-        await ctx.send(embed=ms)
+        gwLogic = round(self.bot.latency * 1000)
+        await ping.edit(content=f'Done: `{msLogic}` (gw: `{gwLogic}`)')
 
   @commands.command()
   async def invite(self, ctx):

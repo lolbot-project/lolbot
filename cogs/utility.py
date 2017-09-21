8da8bf7bbaed8bf1e297c5a4cc9481b2ce1a4a41
S Stewart <iamtheworst@programmer.net>
2017-09-21 17:12:08 -0500

    cogs[utility]: build a border

--- cogs/utility.py
+++ cogs/utility.py
@@ -22,8 +22,8 @@ class Etc:
         d, h = divmod(h, 24)
         upEm = discord.Embed(title='Uptime', colour=0x690E8)
         startedOn = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(self.bot.init_time))
-        upEm.add_field(name='Started on', value=startedOn + '\n')
-        upEm.add_field(name='Uptime', value=f'{d} days, {h} hours, {m} minutes and {s} seconds')
+        upEm.add_field(name='Started on', value=startedOn + '\n |')
+        upEm.add_field(name='Uptime', value=f' {d} days, {h} hours, {m} minutes and {s} seconds')
         await ctx.send(embed=upEm)
 
     @commands.command()

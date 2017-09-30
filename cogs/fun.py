14d26128dcfaceeed85110450ec04bdf9faa2db0
S Stewart <iamtheworst@programmer.net>
2017-09-29 19:49:35 -0500

    cogs[fun]: Make real shit

--- cogs/fun.py
+++ cogs/fun.py
@@ -21,12 +21,18 @@ class Fun:
                 await ctx.send(embed=em)
 
     @commands.command()
-    async def httpcat(self, ctx, *, http_id: str):
+    async def httpcat(self, ctx, http_id: int):
         """http.cat images - ^httpcat <http code>"""
-        httpcat_em = discord.Embed(name='http.cat', colour=0x690E8)
-        httpcat_em.set_image(url='https://http.cat/' + http_id + '.jpg')
-        httpcat_em.set_footer(text='If there is no image, it doesn\'t exist.')
-        await ctx.send(embed=httpcat_em)
+        codes = [100, 101, 200, 201, 202, 204, 206, 207, 300, 301, 302, 303, 304, 305, 307, 400,
+                401, 402, 403, 404, 405, 406, 408, 409, 410, 411, 412, 413, 414, 416, 417, 418, 420,
+                421, 422, 423, 424, 425, 426, 429, 444, 450, 451, 500, 502, 503, 504, 506, 507, 508,
+                509, 511, 599]
+        if http_id in codes:
+            httpcat_em = discord.Embed(name='http.cat', colour=0x690E8)
+            httpcat_em.set_image(url=f'https://http.cat/{http_id}.jpg')
+            await ctx.send(embed=httpcat_em)
+        else:
+            return raise commands.BadArgument('Specified HTTP code invalid')
 
     @commands.command()
     async def dog(self, ctx):

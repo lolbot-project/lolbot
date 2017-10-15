e6eb8cfc9fd17d63c7594faba6945b1b35aa657e
S Stewart <iamtheworst@programmer.net>
2017-10-15 11:11:31 -0500

    utils[websetup]: move websetup script to utils, fix jquery spelling

--- bot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# CORE BOT FILE, DO NOT REMOVE
-# IF YOU DO, RESTART WILL BREAK GUARANTEED
-# (c) 2017 S Stewart
-python3.6 index.py
-
--- utils/setup_website.sh
+++ utils/setup_website.sh
@@ -60,8 +60,8 @@ else
 	echo "Downloading jquery.min.js"
 	wget -O js/jquery.js "https://cdn.jsdelivr.net/npm/jquery@3.2.1/dist/jquery.min.js" > /dev/null 2>&1
 	if [ "$?" -eq 1 ]; then
-	    echo "| JQuery download error. |"
+	    echo "| jQuery download error. |"
 	else
-	    echo "| JQuery download finished. |"
+	    echo "| jQuery download finished. |"
 	fi
 fi

3e516ea338d138d882bc28176e9279683e5a8ee9
S Stewart <iamtheworst@programmer.net>
2017-11-16 17:20:33 -0600

    cogs[utility]: add uptime to stats

--- cogs/utility.py
+++ cogs/utility.py
@@ -108,6 +108,7 @@ class Etc:
         statEmbed.add_field(name='Python', value=sys.version)
         statEmbed.add_field(name='discord.py', value=discord.__version__)
         statEmbed.add_field(name='Servers', value=f'{len(self.bot.guilds)}')
+        statEmbed.add_field(name='Uptime', value=await bot_uptime(self.bot.init_time))
         statPool = ['What have you done now?', 'Why should I do this again?', 'Oh..',
                     'Where did the RAM go?', 'grumble grumble', 'Please hold.', 'No, just, no.',
                     'Have you tried rebooting?', 'memework makes the dreamwork!', 'cool and good']

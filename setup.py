4a13941092c584e61e389d1896353a01dec31a32
S Stewart <tilda@users.noreply.github.com>
2017-12-08 21:20:47 -0600

    setup: use common.py version

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
+from cogs.common import version
 
-setup(name='lolbot', version='1.0', description='A fun bot',
+setup(name='lolbot', version=version, description='A fun bot',
       url='https://lolbot.banne.club', author='S Stewart',
       python_requires='>=3.6')

63d7f2f5ae279486d6e9c92f61d8925b54ee5ec0
S Stewart <iamtheworst@programmer.net>
2017-12-10 21:34:30 -0600

    cogs[osu]: more shit

--- cogs/osu.py
+++ cogs/osu.py
@@ -7,12 +7,12 @@ class Osu:
         self.bot = bot
         self.api = OsuApi(bot.config['osu'])
 
-   @commands.group()
-   async def osu(self, ctx):
-       if ctx.invoked_subcommand is None or 'help':
-           osu_embed = discord.Embed(title='Commands for osu!', colour=0x690E8)
-           await ctx.send(embed=osu_embed)
-           del osu_embed
+    @commands.group()
+    async def osu(self, ctx):
+        if ctx.invoked_subcommand is None or 'help':
+            osu_embed = discord.Embed(title='Commands for osu!', colour=0x690E8)
+            await ctx.send(embed=osu_embed)
 
 def setup(bot):
     bot.add_cog(Osu(bot))
+                    

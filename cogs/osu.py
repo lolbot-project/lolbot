132dc10751fcd7a5e39137ff3ce387e15515ee81
S Stewart <iamtheworst@programmer.net>
2017-12-10 21:37:10 -0600

    cogs[osu], core[cogs]: add osu to load, more fuck

--- cogs/osu.py
+++ cogs/osu.py
@@ -1,18 +1,13 @@
-from osuapi import OsuApi, AHConnector # osu! api wrapper, with a aiohttp connector (backend)
 import discord
 from discord.ext import commands
 
 class Osu:
     def __init__(self, bot):
         self.bot = bot
-        self.api = OsuApi(bot.config['osu'])
 
     @commands.group()
     async def osu(self, ctx):
-        if ctx.invoked_subcommand is None or 'help':
-            osu_embed = discord.Embed(title='Commands for osu!', colour=0x690E8)
-            await ctx.send(embed=osu_embed)
+        await ctx.send(embed=discord.Embed(title='Commands for osu!', colour=0x690E8))
 
 def setup(bot):
     bot.add_cog(Osu(bot))
-                    

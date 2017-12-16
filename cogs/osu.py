279a8c9af3fc8b3b99ae941c734455ce68f52d04
S Stewart <iamtheworst@programmer.net>
2017-12-16 12:36:31 -0600

    cogs[osu]: functional get_user

--- cogs/osu.py
+++ cogs/osu.py
@@ -1,14 +1,28 @@
 import discord
 from discord.ext import commands
+from osuapi import OsuApi, AHConnector
+import osuapi.enums
+import utils.errors
 
 class Osu:
     def __init__(self, bot):
         self.bot = bot
+        if bot.config['osu']:
+            self.api = OsuApi(bot.config['osu'], connector=AHConnector())
+        else:
+            self.api = None
 
     @commands.group()
     async def osu(self, ctx):
         if ctx.invoked_subcommand is None or 'help':
             await ctx.send(embed=discord.Embed(title='Commands for osu!', colour=0x690E8))
+                    
+    @osu.group()
+    async def user(self, ctx, u, mode=osuapi.enums.OsuMode.osu):
+        if self.api:
+            user = await self.api.get_user(u, mode)
+        else:
+            raise utils.errors.ServiceError('osu! api key not configured')
 
 def setup(bot):
     bot.add_cog(Osu(bot))

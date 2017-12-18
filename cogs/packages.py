8f4f95371e21602631b774ec17b38299e0397044
S Stewart <iamtheworst@programmer.net>
2017-12-18 12:16:42 -0600

    cogs[packages]: pkg_url

--- cogs/packages.py
+++ cogs/packages.py
@@ -5,13 +5,14 @@ from cogs import common
 class Packages:
     def __init__(self, bot):
         self.bot = bot
-        def pkg_url(pkg):
-            """Returns the URL for JSON data about a package on PyPI."""
-            return f'https://pypi.python.org/pypi/{pkg}/json'
+        
+    def pkg_url(self, pkg):
+        """Returns the URL for JSON data about a package on PyPI."""
+        return f'https://pypi.python.org/pypi/{pkg}/json'
 
     @commands.command()
     async def pypi(self, ctx, pkg: str):
-        async with ctx.bot.session.get(pkg_url(pkg), headers=common.user_agent) as ps:
+        async with ctx.bot.session.get(self.pkg_url(pkg), headers=common.user_agent) as ps:
             pjson = await ps.json()
             pkg_s = discord.Embed(title=f'PyPI stats for {pkg}', colour=0x690E8)
             pkg_s.add_field(name='Version', value=pjson['info']['version'])

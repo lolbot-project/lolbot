af7a82f8ddf507eeeb1b13a60fc81c4894c50e30
tilda <iamtheworst@programmer.net>
2017-12-27 10:33:18 -0600

    cogs[packages]: I'm lazy

--- cogs/packages.py
+++ cogs/packages.py
@@ -6,16 +6,21 @@ class Packages:
     def __init__(self, bot):
         self.bot = bot
         
-    def pkg_url(self, pkg):
+    def json_pkg(self, pkg):
         """Returns the URL for JSON data about a package on PyPI."""
         return f'https://pypi.python.org/pypi/{pkg}/json'
 
+    def pkg_url(self, pkg):
+        """Returns the URL for a package on PyPI."""
+        return f'https://pypi.python.org/pypi/{pkg}'
+
     @commands.command()
     async def pypi(self, ctx, pkg: str):
-        async with ctx.bot.session.get(self.pkg_url(pkg), headers=common.user_agent) as ps:
+        async with ctx.bot.session.get(self.json_pkg(pkg), headers=common.user_agent) as ps:
             pjson = await ps.json()
-            pkg_s = discord.Embed(title=f'PyPI stats for {pkg}', colour=0x690E8)
+            pkg_s = discord.Embed(title=pkg, colour=0x690E8)
             pkg_s.add_field(name='Version', value=pjson['info']['version'])
+            pk
             await ctx.send(embed=pkg_s)
 
 def setup(bot):

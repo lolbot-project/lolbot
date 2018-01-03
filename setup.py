50be6e6ffa5f3fa264ecc0be4f43528d00e05440
S Stewart <tilda@users.noreply.github.com>
2018-01-03 12:02:17 -0600

    setup: FUCK

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup
-from cogs.common import version
 
-setup(name='lolbot', version=version, description='A fun bot',
+setup(name='lolbot', version='2', description='A fun bot',
       url='https://lolbot.banne.club', author='S Stewart',
       python_requires='>=3.6')

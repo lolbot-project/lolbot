635c08365ef8bb24718df5b771110e91758f1032
S Stewart <tilda@users.noreply.github.com>
2018-03-30 15:11:43 -0500

    setup: update url

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
 
 setup(name='lolbot', version='2', description='A fun bot',
-      url='https://lolbot.banne.club', author='S Stewart',
+      url='https://lolbot.lmao.tf', author='S Stewart',
       python_requires='>=3.6')

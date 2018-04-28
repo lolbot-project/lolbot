680a9a2293e5c7cd3c078eb34785a84dd4e607bb
S Stewart <iamtheworst@programmer.net>
2018-04-28 17:08:30 -0500

    systemd, cogs[owner]: revamp

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,3 +1,13 @@
+import os
+import traceback
+import asyncio
+import logging
+from cogs import common
+# noinspection PyPackageRequirements
+import discord
+# noinspection PyPackageRequirements
+from discord.ext import commands
+from cogs.utils import paste
 """
 The MIT License (MIT)
 
@@ -26,17 +36,7 @@ Owner stuff
 
 Originally made by luna for Jose, the best bot
 """
-import os
-import traceback
-import asyncio
-import logging
-# noinspection PyPackageRequirements
-import discord
 
-# noinspection PyPackageRequirements
-from discord.ext import commands
-
-from cogs.utils import paste
 
 async def run_cmd(cmd: str) -> str:
     """Runs a subprocess and returns the output."""
@@ -44,6 +44,7 @@ async def run_cmd(cmd: str) -> str:
         cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
     results = await process.communicate()
     return "".join(x.decode("utf-8") for x in results)
+
 
 class Owner:
     def __init__(self, bot):
@@ -55,15 +56,21 @@ class Owner:
         """Change playing status"""
         if ctx.invoked_subcommand is None:
             _help = discord.Embed(title='Subcommands', colour=0x690E8)
-            _help.add_field(name='set', value='Sets playing state. `^game set meme`')
-            _help.add_field(name='clear', value='Clears playing state. `^game clear`')
+            _help.add_field(name='set',
+                            value='Sets playing state. `^game set meme`')
+            _help.add_field(name='clear',
+                            value='Clears playing state. `^game clear`')
             return await ctx.send(embed=_help)
 
     @game.command(name='set')
     async def _set(self, ctx, *, game: str):
         """Sets playing state"""
         try:
-            await self.bot.change_presence(game=discord.Game(name=f'{game} | {ctx.bot.config["prefix"]}help | v1.2', type=1, url='https://twitch.tv/monstercat'))
+            a = discord.Streaming
+            g = a(name=f'{game} | {ctx.bot.config["prefix"]}help |'
+                       f' v{common.version}',
+                  url='https://twitch.tv/monstercat')
+            await self.bot.change_presence(activity=g)
         except Exception:
             await ctx.send(f'```\n{traceback.format_exc()}\n```')
         else:
@@ -73,7 +80,10 @@ class Owner:
     async def _clear(self, ctx):
         """Resets playing state to normal"""
         try:
-            await self.bot.change_presence(game=discord.Game(name=f'{ctx.bot.config["prefix"]}help | v1.2', type=1, url='https://twitch.tv/monstercat'))
+            a = discord.Streaming
+            g = a(name=f'{ctx.bot.config["prefix"]}help | v{common.version}',
+                  url='https://twitch.tv/monstercat')
+            await self.bot.change_presence(activity=g)
         except Exception:
             await ctx.send(f'```\n{traceback.format_exc()}```')
         else:
@@ -90,17 +100,26 @@ class Owner:
     @commands.is_owner()
     async def reboot(self, ctx):
         """Reboots the bot.
-        Can only be used with PM2"""
-        restart_land = discord.Embed(title='Restarting', description='Please wait...', colour=0x690E8)
-        restart_msg = await ctx.send(embed=restart_land)
+        Can only be used with PM2 or systemd"""
+        restart_land = discord.Embed(title='Restarting',
+                                     description='Please wait...',
+                                     colour=0x690E8)
+        re_msg = await ctx.send(embed=restart_land)
         pm2_id = os.environ.get('pm_id')
+        if_systemd = os.environ.get('systemd_supervised')
         if pm2_id:
-            await restart_msg.edit(content='Logging out and restarting. Bye!')
+            await re_msg.edit(content='pm2: :wave: bye!')
             await self.bot.session.close()
             await self.bot.logout()
             await run_cmd(f'pm2 restart {pm2_id}')
+        elif if_systemd:
+            await re_msg.edit(content='systemd: :wave: bye!')
+            await self.bot.session.close()
+            await self.bot.logout()
+            await run_cmd(f'systemctl --user restart lolbot')
         else:
-            await restart_msg.edit(content=':warning: pm2 not detected, invoking `shutdown` command')
+            await re_msg.edit(content=':warning: No supervisor; invoking'
+                              ' `shutdown`')
             await ctx.invoke(self.bot.get_command('shutdown'))
 
     @commands.command(hidden=True)
@@ -168,12 +187,15 @@ class Owner:
     async def feedbackrespond(self, ctx, p: int, *, res: str):
         with ctx.typing():
             person = ctx.bot.get_user(p)
-            fbck = discord.Embed(title='Feedback response', description=res, colour=0x690E8)
+            fbck = discord.Embed(title='Feedback response',
+                                 description=res, colour=0x690E8)
             try:
-                await person.send(f'Hello {person.name}! The author has responded to your feedback.', embed=fbck)
+                await person.send(f'Hello {person.name}! The owner has'
+                                  'responded to your feedback.', embed=fbck)
             except Exception as e:
-                return await ctx.send(f'I probably got blocked. ```py\n{e}\n```')
+                return await ctx.send(f'blocked? ```py\n{e}\n```')
             await ctx.send('Successfully sent response!')
+
 
 def setup(bot):
     bot.add_cog(Owner(bot))

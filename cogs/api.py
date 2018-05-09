8a00d81036947f49a8cdf1fbbe4e16818e7c2616
S Stewart <iamtheworst@programmer.net>
2018-05-08 21:43:59 -0500

    cogs[api]: add individual stats endpoints
    Signed-off-by: S Stewart <iamtheworst@programmer.net>

--- cogs/api.py
+++ cogs/api.py
@@ -1,9 +1,15 @@
+"""
+API endpoints for lolbot.
+If you're looking for the cog that powers the API,
+you are in the wrong place. You are looking for
+api_launcher.py.
+"""
 from quart import Quart, jsonify
 
 app = Quart(__name__)
 app.bot = None
 
-
+# Basic shit
 @app.route("/")
 async def no():
     return jsonify({'no': 'go away'})
@@ -14,10 +20,20 @@ async def wsping():
     return jsonify({'ws': round(app.bot.latency, 3)})
 
 
-@app.route("/api/stats")
-async def stats():
-    return jsonify({
-        'ready': True,
-        'guilds': len(app.bot.guilds),
-        'shards': len(app.bot.shards)
-    })
+# Stats endpoints
+@app.route("/api/stats/<whatever>")
+async def stats(whatever):
+    if whatever is 'guilds':
+        return jsonify({'guilds': len(app.bot.guilds)})
+    elif whatever is 'shards':
+        return jsonify({'shards': len(app.bot.shards)})
+    elif whatever is 'ready':
+        return jsonify({'ready': app.bot.is_ready()})
+    elif whatever is 'all':
+        return jsonify({
+            'ready': app.bot.is_ready(),
+            'guilds': len(app.bot.guilds),
+            'shards': len(app.bot.shards)
+        })
+    else:
+        return jsonify({'error': 404})

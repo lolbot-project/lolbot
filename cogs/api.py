c1324d6005fabbc6ec84e5f6d52824d8a392837e
S Stewart <iamtheworst@programmer.net>
2019-01-19 02:56:10 -0600

    api: try to fix stats thing

--- cogs/api.py
+++ cogs/api.py
@@ -56,7 +56,7 @@ async def stats(whatever):
         return jsonify({'shards': len(app.bot.shards)})
     elif whatever == 'ready':
         return jsonify({'ready': app.bot.is_ready()})
-    elif whatever == 'all' or None:
+    elif whatever == 'all' or '/':
         return jsonify({
             'ready': app.bot.is_ready(),
             'guilds': len(app.bot.guilds),

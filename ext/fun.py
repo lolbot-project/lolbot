e0d06475802f8ec5ce6dfcda91ea6771119cf903
S Stewart <tilda@users.noreply.github.com>
2020-03-26 01:43:25 -0500

    fun: add httpcat

--- ext/fun.py
+++ ext/fun.py
@@ -20,3 +20,17 @@ class Fun(commands.Cog):
                 await ctx.send(embed=embed)
             else:
                 raise ServiceError(f'request failed, http {r.status}')
+
+    @commands.command()
+    async def httpcat(self, ctx, http_id: int):
+        codes = [100, 101, 200, 201, 202, 204, 206, 207, 300, 301, 302,
+                 303, 304, 305, 307, 400, 401, 402, 403, 404, 405, 406,
+                 408, 409, 410, 411, 412, 413, 414, 416, 417, 418, 420,
+                 421, 422, 423, 424, 425, 426, 429, 444, 450, 451, 500,
+                 502, 503, 504, 506, 507, 508, 509, 511, 599]
+        if http_id in codes:
+            embed = discord.Embed(name='http.cat', colour=0x690E8)
+            embed.set_image(url=f'https://http.cat/{http_id}.jpg')
+            await ctx.send(embed=embed)
+        else:
+            raise commands.BadArgument('Invalid HTTP code')

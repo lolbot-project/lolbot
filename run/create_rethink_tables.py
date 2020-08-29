cd97c28c66068b23fd16531fef35b6b613cf2c99
S Stewart <tilda@users.noreply.github.com>
2020-08-29 06:34:15 -0500

    shittily written script for creating rethink tables

---
+++ run/create_rethink_tables.py
@@ -0,0 +1,23 @@
+from rethinkdb import RethinkDB
+from rethinkdb.errors import ReqlOpFailedError
+
+r = RethinkDB()
+tables = ["userconfig"]
+
+conn = r.connect("localhost", 28015)
+
+try:
+    r.db_create('lolbot').run(conn)
+    print("- Created lolbot db")
+except ReqlOpFailedError:
+    print("* lolbot DB creation failed. Most likely already exists")
+conn.use("lolbot")
+
+for table in tables:
+    try:
+        r.table_create(table).run(conn)
+        print(f"- Created {table} table")
+    except ReqlOpFailedError:
+        print(f"* {table} table creation failed. Most likely already exists")
+
+print("- Done!")
